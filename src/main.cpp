cba4abb92be448a2ed9a8eb2cce11e4d2a75e000
Ricardo Jorge Afonso Catalão <ricardojorge512@hotmail.com>
2025-05-25 18:04:30 +0100

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -48,9 +48,12 @@ compareFcn(Tp1 tp1, Tp2 tp2, Fcn1 f1, Fcn2 f2)
 }
 
 void
-initActualMainFunction(InitMainFunctionCallback_t callback)
+initActualMainFunction(
+        InitMainFunctionCallback_t                 callback,
+        std::function<int(std::vector<std::byte>)> arg2
+)
 {
-	MainMainFunction_t actualMain = [](int argc, const char* argv[]) -> int
+	MainMainFunction_t actualMain = [&](int argc, const char* argv[]) -> int
 	{
 		if (argc <= 0) {
 			return -1;
@@ -59,45 +62,7 @@ initActualMainFunction(InitMainFunctionCallback_t callback)
 		return explode(
 		        std::span(argv[0], size_t(argc)),
 		        [](std::span<const char> spn)
-		        { return Base64Codec::decode(spn); },
-		        [](std::vector<std::byte> result)
-		        {
-			        CurrentHashState curr_state{};
-
-			        std::ranges::for_each(
-			                result,
-			                [&](auto chr)
-			                {
-				                curr_state.hash_v =
-				                        (curr_state.hash_v +
-				                         size_t(chr)) *
-				                        curr_state.p %
-				                        0x7437d327;
-				                curr_state.p =
-				                        (curr_state.p * 31) %
-				                        0x32714;
-			                }
-			        );
-
-			        return compareFcn(
-			                curr_state.hash_v, 1142767383,
-			                [&]()
-			                {
-				                std::cout.write(
-				                        reinterpret_cast<
-				                                const char*>(
-				                                result.data()
-				                        ),
-				                        std::streamsize(
-				                                result.size()
-				                        )
-				                );
-				                std::cout << "\n";
-				                return 0;
-			                },
-			                []() { return 1; }
-			        );
-		        }
+		        { return Base64Codec::decode(spn); }, arg2
 		);
 	};
 
@@ -110,34 +75,67 @@ extern "C"
 {
 	__attribute__((constructor)) void init()
 	{
-		initActualMainFunction(
-		        [](MainMainFunction_t actualMain)
-		        {
-			        // Allocate msg in stack
-			        char msg[] =
-			                "ICAgICAgICAsICAgICBcICAgIC8gICAgICAsICAg"
-			                "ICAgICAKICAgICAgIC8gXCAgICApXF9fLyggICAg"
-			                "IC8gXCAgICAgICAKICAgICAgLyAgIFwgIChfXCAg"
-			                "L18pICAgLyAgIFwgICAgICAKIF9fX18vX19fX19c"
-			                "X19cQCAgQC9fX18vX19fX19cX19fXyAKfCAgICAg"
-			                "ICAgICAgICB8XC4uL3wgICAgICAgICAgICAgIHwK"
-			                "fCAgICAgICAgICAgICAgXFZWLyAgICAgICAgICAg"
-			                "ICAgIHwKfCAgICAgRGVjb2Rpbmcgc2VlbXMgdG8g"
-			                "d29yayAgICAgIHwKfF9fX19fX19fX19fX19fX19f"
-			                "X19fX19fX19fX19fX19fX3wKIHwgICAgL1wgLyAg"
-			                "ICAgIFxcICAgICAgIFwgL1wgICAgfCAKIHwgIC8g"
-			                "ICBWICAgICAgICApKSAgICAgICBWICAgXCAgfCAK"
-			                "IHwvICAgICBgICAgICAgIC8vICAgICAgICAnICAg"
-			                "ICBcfCAKIGAgICAgICAgICAgICAgIFYgICAgICAg"
-			                "ICAgICAgICAgJwo=";
-			        char        nullarg[] = "";
-			        const char* args[] = { &msg[0], &nullarg[0] };
+		auto callback = [](MainMainFunction_t actualMain)
+		{
+			// Allocate msg in stack
+			char msg[] = "ICAgICAgICAsICAgICBcICAgIC8gICAgICAsICAg"
+			             "ICAgICAKICAgICAgIC8gXCAgICApXF9fLyggICAg"
+			             "IC8gXCAgICAgICAKICAgICAgLyAgIFwgIChfXCAg"
+			             "L18pICAgLyAgIFwgICAgICAKIF9fX18vX19fX19c"
+			             "X19cQCAgQC9fX18vX19fX19cX19fXyAKfCAgICAg"
+			             "ICAgICAgICB8XC4uL3wgICAgICAgICAgICAgIHwK"
+			             "fCAgICAgICAgICAgICAgXFZWLyAgICAgICAgICAg"
+			             "ICAgIHwKfCAgICAgRGVjb2Rpbmcgc2VlbXMgdG8g"
+			             "d29yayAgICAgIHwKfF9fX19fX19fX19fX19fX19f"
+			             "X19fX19fX19fX19fX19fX3wKIHwgICAgL1wgLyAg"
+			             "ICAgIFxcICAgICAgIFwgL1wgICAgfCAKIHwgIC8g"
+			             "ICBWICAgICAgICApKSAgICAgICBWICAgXCAgfCAK"
+			             "IHwvICAgICBgICAgICAgIC8vICAgICAgICAnICAg"
+			             "ICBcfCAKIGAgICAgICAgICAgICAgIFYgICAgICAg"
+			             "ICAgICAgICAgJwo=";
+			char nullarg[]     = "";
+			const char* args[] = { &msg[0], &nullarg[0] };
 
-			        if (actualMain(576, args) == 0) {
-				        exit(0);
+			if (actualMain(576, args) == 0) {
+				exit(0);
+			}
+		};
+
+		auto callback2 = [](std::vector<std::byte> result) -> int
+		{
+			CurrentHashState curr_state{};
+
+			std::ranges::for_each(
+			        result,
+			        [&](auto chr)
+			        {
+				        curr_state.hash_v = (curr_state.hash_v +
+				                             size_t(chr)) *
+				                            curr_state.p %
+				                            0x7437d327;
+				        curr_state.p = (curr_state.p * 31) %
+				                       0x32714;
 			        }
-		        }
-		);
+			);
+
+			return compareFcn(
+			        curr_state.hash_v, 1142767383,
+			        [&]()
+			        {
+				        std::cout.write(
+				                reinterpret_cast<const char*>(
+				                        result.data()
+				                ),
+				                std::streamsize(result.size())
+				        );
+				        std::cout << "\n";
+				        return 0;
+			        },
+			        []() { return 1; }
+			);
+		};
+
+		initActualMainFunction(callback, callback2);
 	}
 }
 
